ae067b96ef29479b0751e94e24eae906dd28d41f
Eduardo Aboy <eduaboy@gmail.com>
2024-04-12 18:56:20 +0200

    Small improvements

--- src/app/vehicle/vehicle-card/vehicle-card.component.ts
+++ src/app/vehicle/vehicle-card/vehicle-card.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Vehicle } from '../vehicle.model';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vehicle-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './vehicle-card.component.html',
   styleUrl: './vehicle-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
--- src/app/vehicle/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { VehicleService } from './vehicle.service';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { VehicleCardComponent } from './vehicle-card/vehicle-card.component';
 
 @Component({
   selector: 'app-vehicle-list',
   standalone: true,
-  imports: [CommonModule, VehicleCardComponent],
+  imports: [AsyncPipe, VehicleCardComponent],
   providers: [VehicleService],
   templateUrl: './vehicle-list.component.html',
   styleUrl: './vehicle-list.component.scss'
