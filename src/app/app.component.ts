ede79bb90a400834f53aa58431642df22cd38a06
Eduardo Aboy <eduaboy@gmail.com>
2024-04-12 11:05:09 +0200

    Create initial vehicles component & use it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { VehiclesComponent } from './vehicles/vehicles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [VehiclesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
